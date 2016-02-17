365e7b4483333f20e4bf2ddb5b97fa06a0d23490
Anders Hallsten <anders@vanami.com>
2016-02-17 15:33:22 +0000

    Radio

--- Twitter/Bootstrap/View/Helper/FormMultiCheckbox.php
+++ Twitter/Bootstrap/View/Helper/FormMultiCheckbox.php
@@ -117,7 +117,7 @@ class Twitter_Bootstrap_View_Helper_FormMultiCheckbox extends Zend_View_Helper_F
             $optId = $id . '-' . $filter->filter($opt_value);
 
             // Wrap the radios in labels
-            $radio = '<label'
+            $radio = '<div class="checkbox"><label'
                     . $this->_htmlAttribs($label_attribs) . ' for="' . $optId . '">'
                     . '<input type="' . $this->_inputType . '"'
                     . ' name="' . $name . '"'
@@ -128,7 +128,7 @@ class Twitter_Bootstrap_View_Helper_FormMultiCheckbox extends Zend_View_Helper_F
                     . $this->_htmlAttribs($attribs)
                     . $endTag
                     . '<span>' . $opt_label . '</span>'
-                    . '</label>';
+                    . '</label></div>';
 
             // add to the array of radio buttons
             $list[] = $radio;
@@ -141,3 +141,4 @@ class Twitter_Bootstrap_View_Helper_FormMultiCheckbox extends Zend_View_Helper_F
     }
 
 }
+
